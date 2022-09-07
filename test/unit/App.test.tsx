e950d9c83f1677b944e5b0cfdf4a2db45a515da4
Johans Neira <johansneirap@gmail.com>
2022-09-06 23:15:29 -0300

    fix: use for absolute paths starting from src

--- test/unit/App.test.tsx
+++ test/unit/App.test.tsx
@@ -1,8 +1,8 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
-import App from '../../src/App';
+import App from 'App';
 
-it('renders hello message', () => {
+it('initial test', () => {
   const { container } = render(<App />);
   expect(container).toMatchSnapshot();
 });
